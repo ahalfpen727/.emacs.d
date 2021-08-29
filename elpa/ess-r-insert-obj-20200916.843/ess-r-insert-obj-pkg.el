57d590b1cc62dc61f7ec49b77311d083947be0bd
ahalfpen727 <andrew.s.judell@gmail.com>
2021-08-29 08:00:53 -0400

    changes?

---
+++ elpa/ess-r-insert-obj-20200916.843/ess-r-insert-obj-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ess-r-insert-obj" "20200916.843" "Insert objects in ESS-R" '((emacs "26.1") (ess "18.10.1")) :commit "f6731eb26dc0fc5b7ca1fa881a5f9100f8fcf494" :authors '(("Shuguang Sun" . "shuguang79@qq.com")) :maintainer '("Shuguang Sun" . "shuguang79@qq.com") :keywords '("tools") :url "https://github.com/ShuguangSun/ess-r-insert-obj")
